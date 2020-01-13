516592d7d51921c7d78321fd3f95cb7c968e0e02
sashazhen <59682769+sashazhen@users.noreply.github.com>
2020-01-13 11:42:20 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,8 @@ import VueRouter from 'vue-router'
 import login from '../views/login.vue'
 import Admin from '../views/admin.vue'
 import HelloWorld from '../components/HelloWorld.vue'
-import  Servercreate from '../views/serve/Servecreate.vue'
+import Servercreate from '../views/serve/Servecreate.vue'
+import SaleChanceList from '../views/sale/SaleChanceList.vue'
 
 Vue.use(VueRouter)
 
@@ -16,19 +17,20 @@ const routes = [{
 		path: '/admin',
 		name: 'Admin',
 		component: Admin,
-		children:[{
-			path:'/admin/',
-			name:'welcome',
-			component:HelloWorld
-		},{
-			path:'/admin/servercreate',
-			name:'Servercreate',
-			component:Servercreate
-		}
-		]
-	},
-	
-
+		children: [{
+			path: '/admin/',
+			name: 'welcome',
+			component: HelloWorld
+		}, {
+			path: '/admin/servercreate',
+			name: 'Servercreate',
+			component: Servercreate
+		}, {
+			path: '/admin/saleChanceList',
+			name: 'SaleChanceList',
+			component: SaleChanceList
+		}]
+	}
 ]
 
 const router = new VueRouter({
